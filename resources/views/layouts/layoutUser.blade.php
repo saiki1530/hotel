7db30902c93ba9f08defd43fe1c5007d31c2f239
saiki1530 <quang.sang1530@gmail.com>
2023-09-15 21:43:07 +0700

    datn

--- resources/views/layouts/layoutUser.blade.php
+++ resources/views/layouts/layoutUser.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" href="/image/favicon.png" type="/image/png">
     <title>@yield('title')</title>
     <!-- Bootstrap CSS -->
-      {{-- <link rel="stylesheet" href="/css/_checkout.css"> --}}
+      <link rel="stylesheet" href="/css/_checkout.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
